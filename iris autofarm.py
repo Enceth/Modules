e9e328bb7df73a58af48b6ce6648546ab1d0da51
Enceth <127540592+HomakB@users.noreply.github.com>
2025-01-06 22:32:42 +0200

    Add files via upload

---
+++ iris autofarm.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from telethon.tl.types import Message
+from .. import loader, utils
+
+@loader.tds
+class IrisFarm(loader.Module):
+    """Автофарм с помощью автоотправки """
+
+    strings = {
+        "name": "IrisFarm",
+    }
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.iris_bot = 5443619563  
+
+    @loader.command()
+    async def autofarm(self, message: Message):
+        """Запускает автофарм"""
+        await utils.answer(message, "Запускаю автоферму...")
+        
+        for i in range(100):  
+            schedule_time = datetime.now() + timedelta(hours=4 * (i + 1))
+            await self.client.send_message(self.iris_bot, "Ферма", schedule=schedule_time)
+        
+        await utils.answer(message, "Автофарм запущен.")
